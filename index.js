96769f9dcdfe08f237405b8e406365b308d8f40f
Steve <uberkootra@gmail.com>
2022-12-12 14:46:16 -0600

    Add JS and fuctionality to monochrome

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const colorInput = document.getElementById('color-picker');
 let selectedColor = colorInput.value;
-selectedColor = selectedColor.replace("#","")
-console.log(selectedColor)
-
+const modess = document.querySelector("#modes")
+console.log(modess.value)
 const square = document.querySelectorAll(".color-square")
+const span = document.querySelectorAll("span")
 
 square[0].style.backgroundColor=`${selectedColor}`
 
-fetch(`https://www.thecolorapi.com/scheme?hex=${selectedColor}&mode=monochrome&count=4`)
-.then(res => res.json())
-.then(data => {
-  const colors = data.colors.map(el => {return el.hex.value})
-  console.log(colors)
+const btn = document.querySelector("#submit")
+
+btn.addEventListener("click", () => {
+  const colorValue = document.getElementById('color-picker').value;
+  let colorForApi = colorValue.replace("#","")
+  let colorDisplay
+
+  fetch(`https://www.thecolorapi.com/scheme?hex=${colorForApi}&mode=monochrome&count=5`)
+  .then(res => res.json())
+  .then(data => {
+    colorDisplay = data.colors.map(el => {return el.hex.value})
+    colorDisplay.forEach((element,index) => {
+      square[index].style.backgroundColor=`${element}`
+      span[index].innerText=`${element}`
+    });    
+  })
+
 })
