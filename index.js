58bee5cc9b02a6d25c9569d4e8b27980769662bb
Steve <uberkootra@gmail.com>
2022-12-12 15:18:08 -0600

    Use prettier

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
-const colorInput = document.getElementById('color-picker');
+const colorInput = document.getElementById("color-picker");
 let selectedColor = colorInput.value;
-const square = document.querySelectorAll(".color-square")
-const span = document.querySelectorAll("span")
+const square = document.querySelectorAll(".color-square");
+const span = document.querySelectorAll("span");
 
-square[0].style.backgroundColor=`${selectedColor}`
+square[0].style.backgroundColor = `${selectedColor}`;
 
-const btn = document.querySelector("#submit")
+const btn = document.querySelector("#submit");
 
 btn.addEventListener("click", () => {
-  const colorValue = document.getElementById('color-picker').value;
-  let colorForApi = colorValue.replace("#","")
-  const modess = document.querySelector("#modes").value
+  const colorValue = document.getElementById("color-picker").value;
+  let colorForApi = colorValue.replace("#", "");
+  const modess = document.querySelector("#modes").value;
   console.log(modess);
 
-  let colorDisplay
+  let colorDisplay;
 
-  fetch(`https://www.thecolorapi.com/scheme?hex=${colorForApi}&mode=${modess}&count=5`)
-  .then(res => res.json())
-  .then(data => {
-    colorDisplay = data.colors.map(el => {return el.hex.value})
-    colorDisplay.forEach((element,index) => {
-      square[index].style.backgroundColor=`${element}`
-      span[index].innerText=`${element}`
-    });    
-  })
-
-})
+  fetch(
+    `https://www.thecolorapi.com/scheme?hex=${colorForApi}&mode=${modess}&count=5`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      colorDisplay = data.colors.map((el) => {
+        return el.hex.value;
+      });
+      colorDisplay.forEach((element, index) => {
+        square[index].style.backgroundColor = `${element}`;
+        span[index].innerText = `${element}`;
+      });
+    });
+});
